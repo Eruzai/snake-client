e6de276c1d35e7680a2a43209be70ac6f83a283e
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-22 15:03:18 -0800

    now uses constants for movement and messages

--- input.js
+++ input.js
@@ -1,29 +1,24 @@
+const { MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, MESSAGES } = require("./constants");
 let connection; // stores the active TCP object
 
 const handleUserInput = function (data) {
   if (data === '\u0003') { // terminates process if ctr + C is pressed
     process.exit();
   }
-  if (data === 'w') { // moves snake up if w key is pressed
+  if (data === MOVE_UP_KEY) { // moves snake up if w key is pressed
     connection.write("Move: up");
   }
-  if (data === 'a') { // moves snake left if a key is pressed
+  if (data === MOVE_LEFT_KEY) { // moves snake left if a key is pressed
     connection.write("Move: left");
   }
-  if (data === 's') { // moves snake down if s key is pressed
+  if (data === MOVE_DOWN_KEY) { // moves snake down if s key is pressed
     connection.write("Move: down");
   }
-  if (data === 'd') { // moves snake right if d key is pressed
+  if (data === MOVE_RIGHT_KEY) { // moves snake right if d key is pressed
     connection.write("Move: right");
   }
-  if (data === 'j') {
-    connection.write("Say: sssnack");
-  }
-  if (data === 'k') {
-    connection.write("Say: need ssspace");
-  }
-  if (data === 'l') {
-    connection.write("Say: sssad");
+  if (MESSAGES.hasOwnProperty(data)) {
+    connection.write("Say: " + MESSAGES[data]);
   }
 };
 
