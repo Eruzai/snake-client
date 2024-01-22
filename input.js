766e5cb6c73562615c3befce14958e33522bf93d
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-22 12:58:58 -0800

    console.log a message to the terminal for movement using wasd keys

--- input.js
+++ input.js
@@ -2,6 +2,18 @@ const handleUserInput = function (data) {
   if (data === '\u0003') { // terminates process if ctr + C is pressed
     process.exit();
   }
+  if (data === 'w') { // moves snake n if x key is pressed
+    console.log("Move: up");
+  }
+  if (data === 'a') { // moves snake n if x key is pressed
+    console.log("Move: left");
+  }
+  if (data === 's') { // moves snake n if x key is pressed
+    console.log("Move: down");
+  }
+  if (data === 'd') { // moves snake n if x key is pressed
+    console.log("Move: right");
+  }
 };
 
 const setupInput = function () { // setup interface to handle user input from stdin
