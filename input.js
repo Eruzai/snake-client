985109237c6d51f1bbfc77ae717dc6212535e829
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-22 15:24:44 -0800

    movement now accesses an object

--- input.js
+++ input.js
@@ -1,25 +1,16 @@
-const { MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, MESSAGES } = require("./constants");
+const { /*MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY,*/MOVEMENT, MESSAGES } = require("./constants");
 let connection; // stores the active TCP object
 
 const handleUserInput = function (data) {
   if (data === '\u0003') { // terminates process if ctr + C is pressed
     process.exit();
-  }
-  if (data === MOVE_UP_KEY) { // moves snake up if w key is pressed
-    connection.write("Move: up");
-  }
-  if (data === MOVE_LEFT_KEY) { // moves snake left if a key is pressed
-    connection.write("Move: left");
-  }
-  if (data === MOVE_DOWN_KEY) { // moves snake down if s key is pressed
-    connection.write("Move: down");
-  }
-  if (data === MOVE_RIGHT_KEY) { // moves snake right if d key is pressed
-    connection.write("Move: right");
-  }
+  };
+  if (MOVEMENT.hasOwnProperty(data)) {
+    connection.write("Move: " + MOVEMENT[data]);
+  };
   if (MESSAGES.hasOwnProperty(data)) {
     connection.write("Say: " + MESSAGES[data]);
-  }
+  };
 };
 
 const setupInput = (conn) => { // setup interface to handle user input from stdin
