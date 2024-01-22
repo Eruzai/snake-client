eb23ba48db081c9dd0db5614b1946acfdf1467ef
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-22 14:03:19 -0800

    accepts connection information, assigns it to a global variable, writes movement data to the variable

--- input.js
+++ input.js
@@ -1,28 +1,31 @@
+let connection; // stores the active TCP object
+
 const handleUserInput = function (data) {
   if (data === '\u0003') { // terminates process if ctr + C is pressed
     process.exit();
   }
-  if (data === 'w') { // moves snake n if x key is pressed
-    console.log("Move: up");
+  if (data === 'w') { // moves snake up if w key is pressed
+    connection.write("Move: up");
   }
-  if (data === 'a') { // moves snake n if x key is pressed
-    console.log("Move: left");
+  if (data === 'a') { // moves snake left if a key is pressed
+    connection.write("Move: left");
   }
-  if (data === 's') { // moves snake n if x key is pressed
-    console.log("Move: down");
+  if (data === 's') { // moves snake down if s key is pressed
+    connection.write("Move: down");
   }
-  if (data === 'd') { // moves snake n if x key is pressed
-    console.log("Move: right");
+  if (data === 'd') { // moves snake right if d key is pressed
+    connection.write("Move: right");
   }
 };
 
-const setupInput = function () { // setup interface to handle user input from stdin
-const stdin = process.stdin;
-stdin.setRawMode(true);
-stdin.setEncoding("utf8");
-stdin.resume();
-stdin.on("data", handleUserInput); // accepts keyboard input
-return stdin;
+const setupInput = (conn) => { // setup interface to handle user input from stdin
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput); // accepts keyboard input
+  return stdin;
 };
 
 module.exports = {
