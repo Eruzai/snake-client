2c23bdfc460f3a70a0387f56dfe1976a0daa208b
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-23 13:11:19 -0800

    removed code for interval to keep snake moving, found setting for this in server files

--- input.js
+++ input.js
@@ -1,18 +1,13 @@
-const { clearInterval } = require("timers");
 const { MOVEMENT, MESSAGES } = require("./constants");
 
 let connection; // stores the active TCP object
-let interval = undefined; // stores continous movement interval ID
 
 const handleUserInput = function(data) {
   if (data === '\u0003') { // terminates process if ctr + C is pressed
     process.exit();
   }
-  if (MOVEMENT.hasOwnProperty(data)) { // checks object for key pressed to determine if keybind was pressed
-    clearInterval(interval); // clears our interval for continuous movement
-    interval = setInterval(() => { // creates an interval for continous movement in the direction specified by keybind pressed
-      connection.write("Move: " + MOVEMENT[data]); // tells the server in which direction to move our snake
-    }, 100);
+  if (MOVEMENT.hasOwnProperty(data)) { // checks object for key pressed to determine if keybind was pressed 
+    connection.write("Move: " + MOVEMENT[data]); // tells the server in which direction to move our snake
   }
   if (MESSAGES.hasOwnProperty(data)) { // checks object for key pressed to determine if keybind was pressed
     connection.write("Say: " + MESSAGES[data]); // sends message for server to display corresponding to our keybind
