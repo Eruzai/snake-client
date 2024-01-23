54226fc6e607281863b3ead2ad987cc1b37e7cc3
Mathew Levasseur <5962839+matlev@users.noreply.github.com>
2024-01-23 15:38:45 -0800

    Send bitFlags to server

--- client.js
+++ client.js
@@ -1,10 +1,22 @@
 const net = require("net");
-const { IP, PORT, NAME } = require("./constants");
+const { IP, PORT, NAME, OPTIONS } = require("./constants");
 
 const connect = function() { // establishes a connection with the game server
+
+  // This can be modified to also allow user-input flags from the command line,
+  // and make them override constants.js default values.
+  // ie. bitFlags |= process.argv[key] || val;
+  // Proper sanitization and validation of arg inputs should be done before hand.
+  let bitflags = 0;
+  for (const [key, val] of Object.entries(OPTIONS)) {
+    bitFlags <<= 1;  // Shift bit value left by 1.  
+    bitFlags |= val; // Sets the right-most bit to 1 if flag is true, 0 otherwise.
+  }
+  
   const conn = net.createConnection({
     host: IP, // IP address here,
-    port: PORT // PORT number here,
+    port: PORT, // PORT number here,
+    options: bitFlags // Server code needs to be updated to handle this.
   });
 
   conn.setEncoding("utf8"); // interpret incoming data as text
